985d0fb50401806b5edcadc6ff383e0a14633dc8
ShuyangenFrance <50994536+ShuyangenFrance@users.noreply.github.com>
2023-04-10 22:08:08 +0200

    Add files via upload
    First commit

---
+++ main.py
@@ -0,0 +1,64 @@
+import os
+import pickle
+
+from google.auth.transport.requests import Request
+
+from google_auth_oauthlib.flow import InstalledAppFlow
+from llama_index import (
+    GPTSimpleVectorIndex,
+    download_loader,
+    SimpleDirectoryReader,
+    LLMPredictor,
+    ServiceContext,
+)
+
+from langchain.chat_models import ChatOpenAI
+
+
+def authorize_gdocs():
+    google_oauth2_scopes = [
+        "https://www.googleapis.com/auth/documents.readonly"
+    ]
+    cred = None
+    if os.path.exists("token.pickle"):
+        with open("token.pickle", 'rb') as token:
+            cred = pickle.load(token)
+    if not cred or not cred.valid:
+        if cred and cred.expired and cred.refresh_token:
+            cred.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file("credentials.json", google_oauth2_scopes)
+            cred = flow.run_local_server(port=0)
+        with open("token.pickle", 'wb') as token:
+            pickle.dump(cred, token)
+
+def get_index():
+    # define LLM
+    llm_predictor = LLMPredictor(llm=ChatOpenAI(temperature=0.1, model_name="gpt-3.5-turbo"))
+    service_context = ServiceContext.from_defaults(llm_predictor=llm_predictor)
+
+    index = GPTSimpleVectorIndex.from_documents(
+        documents, service_context=service_context
+    )
+    return index
+
+if __name__ == '__main__':
+
+    os.environ['OPENAI_API_KEY'] = "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"
+
+    # change knowledge base resource here:
+    kb="google"
+    if kb == "google":
+        authorize_gdocs()
+        GoogleDocsReader = download_loader('GoogleDocsReader')
+        gdoc_ids = ['1RdVxR0OvU-x1Lkj1gLqpsPG5a9mtwocduuYaZXEsVxo',
+                    '1nYqUejwJoLa5vJ1h-FXo3xrnrV9BS4RBZdOZ04L3wMA']
+        loader = GoogleDocsReader()
+        documents = loader.load_data(document_ids=gdoc_ids)
+    elif kb=="local":
+        documents= SimpleDirectoryReader('data').load_data()
+    index=get_index()
+
+    query = "What is the limitation of CSVs?"
+    response = index.query(query, response_mode="compact")
+    print(response)
